3d4e7d4ee41f02a8fe0b6c97f713fc125ef844b8
SamuelCoder1 <161090562+SamuelCoder1@users.noreply.github.com>
2024-08-16 21:22:05 -0500

    Delete academia directory

--- academia/src/main/java/com/riwi/academia/repositories/TareaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.riwi.academia.repositories;
-
-import com.riwi.academia.models.Tarea;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TareaRepository extends JpaRepository<Tarea, Integer> {
-}
